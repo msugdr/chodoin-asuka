7f3a9ce9eb41e780f933452a30261b450bca17bd
msugdr <msugdr@gmail.com>
2023-05-10 11:21:52 +0900

    over all

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-// 2023.5.9 chodoin-asuka
+// 2023.5.10 chodoin-asuka
 const http = require('http');
 const fs = require('fs');
-var msg;
 var html;
 var dummy;
 var filename;
 const server = http.createServer((request, response) => { 
-    [dummy,filename]=request.url.split("/");
-    [file,ext]=filename.split(".");
-    if (ext == 'html'){
-        fs.readFile(filename,'utf8', function(err, data) {html=data});
-        response.writeHead(200, {"Content-Type": "text/html"});
-        msg=html;
-        response.end(msg);
-        console.log(" URI : " + request.url + " DUMMY=" + dummy + "  filename=" + filename); 
+    if (request.method === 'GET'){
+        [dummy,filename]=request.url.split("/");
+        [file,ext]=filename.split(".");
+        if (ext.match(/html|css|javascript/) ){
+            response.writeHead(200, {"Content-Type": `text/${ext}`});
+            html=fs.readFileSync(filename,'utf8');
+            response.end(html);
+            console.log(" URI : " + request.url + " DUMMY=" + dummy + "  filename=" + filename); 
+        }
+        if (ext.match(/png|jpg|gif/) ){
+            response.writeHead(200, {"Content-Type": `img/${ext}`});
+            var image = fs.readFileSync(filename, "binary");
+            response.end(image,"binary");
+            console.log(" URI= " + request.url + " DUMMY=" + dummy + "  filename=" + filename); 
+        }
+    } else if (request.method === 'POST'){
+        var data = '';
+    
+        //POSTデータを受けとる
+        request.on('data', function(chunk) {data += chunk})
+           .on('end', function() {
+            response.writeHead(200, {"Content-Type": "text/html"});
+             console.log(data);
+             response.end(data);
+            })
     }
 });
 const port = process.env.PORT || 1337;
