a6cba781611ba5bf5a039ec2fc7f469af2685fdc
msugdr <99190980+msugdr@users.noreply.github.com>
2023-05-11 18:44:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,14 +13,12 @@ const server = http.createServer((request, response) => {
             response.writeHead(200, {"Content-Type": `text/${ext}`});
             html=fs.readFileSync(filename,'utf8');
             response.end(html);
-            console.log(" URI : " + request.url + " DUMMY=" + dummy + "  filename=" + filename); 
         }
 //        if (ext.match(/png|jpg|gif/) ){
           if (ext==="gif" || ext==="jpg" || ext==="png" ){
             response.writeHead(200, {"Content-Type": `img/${ext}`});
             var image = fs.readFileSync(filename, "binary");
-            response.end(image,"binary");
-//            console.log(" URI= " + request.url + " DUMMY=" + dummy + "  filename=" + filename); 
+            response.end(image,"binary"); 
         } 
         if (ext==="txt" || ext==="ico" ){
                 response.writeHead(200, {"Content-Type": "text/html"});      
@@ -34,11 +32,9 @@ const server = http.createServer((request, response) => {
         request.on('data', function(chunk) {data += chunk})
            .on('end', function() {
             response.writeHead(200, {"Content-Type": "text/html"});
-             console.log(data);
              response.end(data);
             })
     }
 });
 const port = process.env.PORT || 1337;
 server.listen(port);
-//console.log("Server running at http://localhost:%d", port);
