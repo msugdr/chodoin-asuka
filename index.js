f59cdee0c26251ff462beb8a60a3ef45ac019d3b
msugdr <99190980+msugdr@users.noreply.github.com>
2023-05-11 14:56:12 +0900

    Update index.js no match

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ const server = http.createServer((request, response) => {
     if (request.method === 'GET'){
         [dummy,filename]=request.url.split("/");
         [file,ext]=filename.split(".");
-        if (ext.match(/html|css|javascript/) ){
+//        if (ext.match(/html|css|javascript/) ){
+         if (ext==="html" || ext === "css"){
             response.writeHead(200, {"Content-Type": `text/${ext}`});
             html=fs.readFileSync(filename,'utf8');
             response.end(html);
             console.log(" URI : " + request.url + " DUMMY=" + dummy + "  filename=" + filename); 
         }
-        if (ext.match(/png|jpg|gif/) ){
+//        if (ext.match(/png|jpg|gif/) ){
+          if (ext==="gif" || ext==="jpg" || ext==="png" ){
             response.writeHead(200, {"Content-Type": `img/${ext}`});
             var image = fs.readFileSync(filename, "binary");
             response.end(image,"binary");
